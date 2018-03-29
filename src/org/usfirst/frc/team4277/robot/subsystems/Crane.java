91cbe11cb8b919392a317c5ba56e9274b05a5079
NoNameSteve <cooperRoboticsBooster@gmail.com>
2018-03-28 20:06:52 -0500

    Crane updates

--- src/org/usfirst/frc/team4277/robot/subsystems/Crane.java
+++ src/org/usfirst/frc/team4277/robot/subsystems/Crane.java
@@ -3,47 +3,44 @@ package org.usfirst.frc.team4277.robot.subsystems;
 import org.usfirst.frc.team4277.robot.Preferences;
 import org.usfirst.frc.team4277.robot.Robot;
 
-import edu.wpi.first.wpilibj.Victor;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Crane extends Subsystem {
 	
-	Victor climberLauncherMotor;
+	public TalonSRX climberLauncherMotor;
 	
 	public Crane(int launchMotor) {
-		climberLauncherMotor = new Victor(launchMotor);
+		climberLauncherMotor = new TalonSRX(launchMotor);
 	}
 	
 	public void launch () {
-		
-		double craneSpeed = Robot.prefs.getDouble("CraneUpSpeed", 0.9);
-		double craneWait = Robot.prefs.getDouble("CraneWait", 2);
+		double craneSpeed = Robot.prefs.getDouble("CraneUpSpeed", 0.6);
+		//double craneWait = Robot.prefs.getDouble("CraneWait", 2);
 		SmartDashboard.putNumber(Preferences.CRANE_SPEED, craneSpeed);
-		SmartDashboard.putNumber(Preferences.CRANE_WAIT, craneWait);
+		//SmartDashboard.putNumber(Preferences.CRANE_WAIT, craneWait);
 		System.out.println("Crane Launch");
-		//climberLauncherMotor.set(ControlMode.PercentOutput, craneSpeed);
-		climberLauncherMotor.set(craneSpeed);
+		climberLauncherMotor.set(ControlMode.PercentOutput, craneSpeed);
 		//Timer.delay(craneWait);
-		//stop();
-		
+		stop();
 	}
 
 	public void Retract () { //temp name for test
-		double craneSpeed = Robot.prefs.getDouble("CraneRetractSpeed", -0.35);
-		double craneWait = Robot.prefs.getDouble("CraneWait", 2);
+		double craneSpeed = Robot.prefs.getDouble("CraneRetractSpeed", -0.25);
+		//double craneWait = Robot.prefs.getDouble("CraneWait", 2);
 		SmartDashboard.putNumber(Preferences.CRANE_SPEED, craneSpeed);
-		SmartDashboard.putNumber(Preferences.CRANE_WAIT, craneWait);
-		System.out.println("Crane Retract");
-		//climberLauncherMotor.set(ControlMode.PercentOutput, craneSpeed);
-		climberLauncherMotor.set(craneSpeed);
+		//SmartDashboard.putNumber(Preferences.CRANE_WAIT, craneWait);
+		System.out.println("Crane Launch");
+		climberLauncherMotor.set(ControlMode.PercentOutput, craneSpeed);
 		//Timer.delay(craneWait);
-		//stop();
+		stop();
 	}
 	public void stop() {
 		System.out.println("Crane Stop");
-		//climberLauncherMotor.set(ControlMode.PercentOutput, 0.0);
-		climberLauncherMotor.set(0.0);
+		climberLauncherMotor.set(ControlMode.PercentOutput, 0.0);
 	}
 
 	@Override
